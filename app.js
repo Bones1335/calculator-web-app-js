2d13c3be1ada9f8a4c4e977873e941d78d451dd8
Bones <ahansen1335@gmail.com>
2022-01-08 10:16:45 +0100

    numbers populate display

--- app.js
+++ app.js
@@ -32,6 +32,31 @@ const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            console.log(button.id);
+            const clickedButton = button.id;
+            populateDisplay(clickedButton);
         });
     });
+
+function populateDisplay(clickedButton) {
+    if (clickedButton === 'one') {
+        display.textContent = 1;
+    } else if (clickedButton === 'two') {
+        display.textContent = 2;
+    } else if (clickedButton === 'three') {
+        display.textContent = 3;
+    } else if (clickedButton === 'four') {
+        display.textContent = 4;
+    } else if (clickedButton === 'five') {
+        display.textContent = 5;
+    } else if (clickedButton === 'six') {
+        display.textContent = 6;
+    } else if (clickedButton === 'seven') {
+        display.textContent = 7;
+    } else if (clickedButton === 'eight') {
+        display.textContent = 8;
+    } else if (clickedButton === 'nine') {
+        display.textContent = 9;
+    } else if (clickedButton === 'zero') {
+        display.textContent = 0;
+    } 
+};
